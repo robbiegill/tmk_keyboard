4e317c207521c03e48ac10d8cfc4d64ff58fd55c
Robbie Gill <robbie@zenpayroll.com>
2016-04-14 21:03:13 -0700

    theory of fn swap `space` -> `_`

--- keyboard/hhkb/keymap_jp_rdg.c
+++ keyboard/hhkb/keymap_jp_rdg.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * HHKB JP Layout
  */
 #include "keymap_common.h"
@@ -9,31 +9,31 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] __attribute__ ((section (".key
 #else
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 #endif
-    /* Layer 0: Default Layer */
+    /* Layer 0 */
     KEYMAP_JP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, JYEN,BSPC, \
               TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC, \
               LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,BSLS,ENT, \
               LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RO,  UP,  RSFT, \
-              FN0, ZKHK,LGUI,LALT,MHEN,     SPC,      HENK,KANA,RALT,FN0, LEFT,DOWN,RGHT),
+              FN0, LCTL,LALT,LGUI, FN0,     SPC,      BSPC,RSFT,RALT,FN0, LEFT,DOWN,RGHT),
 
-    /* Layer 1: HHKB mode (HHKB Fn)
+    /* Layer 1: hidden layer
      * ,-----------------------------------------------------------.
      * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
      * |-----------------------------------------------------------|
-     * |Caps |   |   |   |   |   |   |   |Psc|Slk|Pus|Up |   |     |
+     * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |
      * |------------------------------------------------------`    |
-     * |      |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|Rig|   |    |
+     * |      |VoD|VoU|Mut|   |   |lft|dwn|up |rgt|   |   |   |    |
      * |-----------------------------------------------------------|
-     * |       |   |   |   |   |   |  +|  -|End|PgD|Dow|   |   |   |
+     * |       |   |   |   |   |   |   |   |   |   |   |   |   |   |
      * |-----------------------------------------------------------|
-     * |   ||   |   |   |   |         |   |   |   |   ||   |   |   |
+     * |   ||   |   |   |   |   (_)   |Del|   |   |   ||   |   |   |
      * `-----------------------------------------------------------'
-     */ 
+     */
     KEYMAP_JP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
-              CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, \
-              TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,TRNS,PENT, \
-              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,TRNS, \
-              TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
+              TAB ,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, TRNS, \
+              TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,LEFT,DOWN,  UP,RGHT,TRNS,TRNS,TRNS,TRNS, \
+              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+              TRNS,TRNS,TRNS,TRNS,TRNS,     FN1,      DEL,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
 };
 
 
@@ -47,4 +47,5 @@ const uint16_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
 const uint16_t fn_actions[] PROGMEM = {
 #endif
     [0] = ACTION_LAYER_MOMENTARY(1),
+    [1] = ACTION_MODS_KEY(MOD_LSFT, KC_MINS), // _ (underscore) -> "shift+-"
 };
